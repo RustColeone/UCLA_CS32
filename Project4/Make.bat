c614f4861193e47aeaf35e355b2c68edbd995510
RustColeone <leoyan01@yahoo.com>
2022-06-14 22:35:13 -0700

    wrapped up

--- Project4/Make.bat
+++ Project4/Make.bat
@@ -1,7 +1,9 @@
+g++ -o generateTests.exe generateTests.cpp
 g++ -o testerHash.exe NameTablevhash.cpp testNameTable.cpp
 g++ -o testerMap.exe NameTablevmap.cpp testNameTable.cpp
 g++ -o testerSlow.exe NameTablevslow.cpp testNameTable.cpp
 
+generateTests.exe
 testerHash.exe
 testerMap.exe
 testerSlow.exe
